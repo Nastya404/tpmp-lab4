110f9f2720078110b3768eee62a8f069c7be4c80
bohushpalina <palinabohush@gmail.com>
2026-03-25 22:57:15 +0300

    Updated Tests

--- t/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/polinwq_534/macOS отчеты/tpmp-lab4/t/_deps/googletest-src
-# Build directory: /home/polinwq_534/macOS отчеты/tpmp-lab4/t/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- t/src/CMakeFiles/autopark.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for autopark.
-# This may be replaced when dependencies are built.
